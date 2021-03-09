fb6aa73aaa28b2f483578575c50393fe34eb8b6d
Michael Fuget <michaelfuget@gmail.com>
2021-03-09 02:37:15 +0000

    Automatically backed up by Learn

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,4 +1,4 @@
-//No need to do any work here.  
+//No need to do any work here.
 //If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
 
 function displayTime() {
@@ -8,7 +8,7 @@ function displayTime() {
       clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
       clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
       clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-  }, 1000)
+  }, 1500)
 
 
 }
